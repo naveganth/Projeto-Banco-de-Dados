4589f1c897a33dfc67276cbe1c3ffc4a0b00990b
Gabriel <pessoal@gabrielataide.com>
2024-04-24 19:04:24 -0300

    Implementação básica do mysql

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use actix_files as fs;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use fs::{Files, NamedFile};
+mod db;
 
 #[get("/")]
 async fn indice() -> actix_web::Result<NamedFile> {
     Ok(NamedFile::open("./paginas/index.html")?)
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let db = db::Banco::new("mysql://root:senhaboa@localhost:9876/projeti");
+    if let Ok(mut banco) = db {
+        println!("Conexão com banco de dados sucedida!");
+        println!("Banco: {:?}", banco);
+
+        banco.verificar_tabelas();
+        banco.criar_tabelas();
+    }
+
     HttpServer::new(|| {
         App::new()
         .service(indice)
