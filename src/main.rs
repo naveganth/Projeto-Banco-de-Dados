9b339c8f9fd2e559364f50d1897d801448546d52
Gabriel <pessoal@gabrielataide.com>
2024-04-24 21:29:45 -0300

    Banco de dados, dessa vez eu li a documentação

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 use actix_files as fs;
+#[allow(unused_imports)]
+use actix_web::http::Error;
+#[allow(unused_imports)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[allow(unused_imports)]
 use fs::{Files, NamedFile};
-mod db;
+use mysql::*;
+#[allow(unused_imports)]
+use mysql::prelude::*;
 
-#[get("/")]
-async fn indice() -> actix_web::Result<NamedFile> {
+
+async fn indice( pool: web::Data<Pool>, ) -> actix_web::Result<NamedFile> {
+    let mut conn = pool.get_conn().expect("Erro ao conectar ao banco de dados");
     Ok(NamedFile::open("./paginas/index.html")?)
 }
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let url = "mysql://root:senhaboa@localhost:9876/projeti";
+    let pool = Pool::new(url).expect("Erro ao conectar ao banco");
 
-    let db = db::Banco::new("mysql://root:senhaboa@localhost:9876/projeti");
-    if let Ok(mut banco) = db {
-        println!("Conexão com banco de dados sucedida!");
-        println!("Banco: {:?}", banco);
-
-        banco.verificar_tabelas();
-        banco.criar_tabelas();
-    }
-
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
-        .service(indice)
+        .app_data(web::Data::new(pool.clone()))
+        .route("/", web::get().to(indice))
         .service(fs::Files::new("/estaticos", "./estaticos"))
     })
     .bind(("0.0.0.0", 9000))?
