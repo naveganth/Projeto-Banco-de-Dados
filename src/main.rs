45751263d57625116698ab02f8f1e21269c42f20
Gabriel <pessoal@gabrielataide.com>
2024-04-25 19:31:34 -0300

    Implementação básica da renderização de dados na página

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use actix_files as fs;
-#[allow(unused_imports)]
 use actix_web::http::Error;
-#[allow(unused_imports)]
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-#[allow(unused_imports)]
 use fs::{Files, NamedFile};
 use mysql::*;
-#[allow(unused_imports)]
 use mysql::prelude::*;
+use serde_json::json;
+use handlebars::{Handlebars, DirectorySourceOptions};
+
+mod modelos;
+mod banco;
 
 
-async fn indice( pool: web::Data<Pool>, ) -> actix_web::Result<NamedFile> {
+async fn indice( pool: web::Data<Pool>, hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    println!("Indice");
     let mut conn = pool.get_conn().expect("Erro ao conectar ao banco de dados");
-    Ok(NamedFile::open("./paginas/index.html")?)
+    
+    let dados = json!({
+        "teste": "adhdfhjdshkesfhdksbaba"
+    });
+
+    println!("Dados: {}", dados);
+    let body = hb.render("index", &dados).unwrap();
+    HttpResponse::Ok().body(body)
 }
 
 
@@ -21,9 +30,20 @@ async fn main() -> std::io::Result<()> {
     let url = "mysql://root:senhaboa@localhost:9876/projeti";
     let pool = Pool::new(url).expect("Erro ao conectar ao banco");
 
+    // Se as tabelas do banco não estiverem certas, corrige elas
+    if !banco::validar_tabelas(&pool) {
+        banco::corrigir_tabelas(&pool);
+    }
+
+    let opcoes_hb = handlebars::DirectorySourceOptions{tpl_extension: String::from(".html"), hidden: false, temporary: false};
+    let mut hb = Handlebars::new();
+    hb.register_templates_directory("./paginas", opcoes_hb).unwrap();
+    let hb_ref = web::Data::new(hb);
+
     HttpServer::new(move || {
         App::new()
         .app_data(web::Data::new(pool.clone()))
+        .app_data(hb_ref.clone())
         .route("/", web::get().to(indice))
         .service(fs::Files::new("/estaticos", "./estaticos"))
     })
