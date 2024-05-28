acdae8aa22652ccb0276fc42ab97123ace405eb3
Gabriel-1201 <pGabriel-1201>
2024-05-28 19:43:24 -0300

    Página de shop

--- loja/views.py
+++ loja/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from .models import *
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     #Pega primeiros quatros produtos
     produtos = Produto.objects.all()
     produtos = produtos[:4]
     
     print("Produtos encontrados:", produtos)
     return render(request, "loja/index.html", {"produtos": produtos})
+
+def shop(request: HttpRequest):
+    produtos = Produto.objects.all().order_by("nome")
+    
+    print("Produtos encontrados:", produtos)
+    return render(request, "loja/shop.html", {"produtos": produtos})
