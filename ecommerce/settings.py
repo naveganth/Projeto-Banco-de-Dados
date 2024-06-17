54593c506cc5fde666bd2508ae75cfb6f8d068d7
Gabriel-1201 <pGabriel-1201>
2024-06-16 22:36:58 -0300

    Amizade encerrada com o SQLite, MySql é meu novo amigo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+          'default': {
+              'ENGINE': 'django.db.backends.mysql',
+              'NAME': 'django',
+              'USER': 'django',
+              'PASSWORD': 'senhasecreta',
+              'HOST': 'servidor.lan',
+              'PORT': '3306',
+          }
+      }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
